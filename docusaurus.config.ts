aba56b678bf8745f5983fe64628039d92d19e49b
Greg Sanders <gregasanders91@gmail.com>
2026-02-04 17:12:11 -0600

    feat: update favicon to use Trajectory logo

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -7,7 +7,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 const config: Config = {
   title: 'Trajectory',
   tagline: 'Self-hosted, privacy-first app for tracking children\'s health data',
-  favicon: 'img/favicon.ico',
+  favicon: 'img/favicon.png',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
